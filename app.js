ece1d9fc9909b499005291f2ec4f85f9f79162b8
Heather Frantisak <hfrantisak@mvnp.com>
2015-08-05 12:07:45 -1000

    Remove unused code.

--- app.js
+++ app.js
@@ -13,17 +13,6 @@ app.get('/mobile.html', function(req, res) {
   res.send('mobile.html');
 });
 
-// var server = http.createServer(function(req, res) {
-//   res.writeHead(200, {
-//     'Content-type': 'text/html; charset=utf-8;'
-//   });
-//   res.end('end response');
-// });
-
-// server.listen(1337);
-
 app.listen(app.get('port'), function() {
-  // var host = server.address().address;
-  // var port = server.address().post;
   console.log('node app running on port ' + app.get('port'));
 });
